bfa0a26e46c374b275c66912e274f142dfb9e8b5
deepa8115 <65079662+deepa8115@users.noreply.github.com>
2020-07-24 11:26:14 +0530

    Create FibonacciSeries.java
    Here, there is a two program of Fibonacci Series 1. Without using recursion and 2. Using recursion

---
+++ FibonacciSeries.java
@@ -0,0 +1,37 @@
+//Fibonacci series without using recursion
+class FibonacciSeries {
+ public static void main(String args[])
+ {
+   int n1=0, n2=1, n3, i, count=10;
+   System.out.println(n1+""+n2);
+   for(i=2; i<count; ++i)
+   {
+     n3=n1+n2;
+     System.out.print(""+n3);
+     n1=n2;
+     n2=n3;
+   }
+ }
+}
+
+//Fibonacci Series using recursion
+class Fibonacci {
+ static int n1=0, n2=1, n3=0;
+ static void printFibonacci(int count)
+ {
+   if(count>0)
+   {
+    n3=n1+n2;
+    n1=n2;
+    n2=n3;
+    System.out.print(""+n3);
+    printFibonacci(count-1);
+   }
+ }
+ public static void main(String args[])
+ {
+  int count=10;
+  System.out.print(n1+""+n2);
+  printFibonacci(count-2);
+ }
+}
